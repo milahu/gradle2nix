06a04fb4e97dea7219f0c07426aaab758b7960af
Milan Hauth <milahu@gmail.com>
2024-10-15 16:15:39 +0200

    add version-catalog-update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     base
     alias(libs.plugins.kotlin.serialization) apply false
+    id("com.github.ben-manes.versions") version "0.41.0"
+    id("nl.littlerobots.version-catalog-update") version "0.8.4"
 }
 
 group = "org.nixos.gradle2nix"
